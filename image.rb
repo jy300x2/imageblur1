673ef482ab11db4631e4f69aaf0cd47d59948ee1
Jon Yu <jy300x2@gmail.com>
2016-06-06 22:57:41 +0000

    Initial commit

---
+++ image.rb
@@ -0,0 +1,26 @@
+class Image
+  attr_writer :arr
+
+  def initialize(arr)
+    self.arr = arr
+  end
+
+  def output_image
+    @arr.each do |row|
+      puts row.join(' ')
+    end
+  end
+
+end
+
+image = Image.new([
+  [0, 0, 0, 0],
+  [0, 1, 0],
+  [0, 1],
+  [0],
+  [0, 0, 0, 1],
+  [0, 0, 0, 0, 0, 0, 1]
+])
+
+image.output_image
+
